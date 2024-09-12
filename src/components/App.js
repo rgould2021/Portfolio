847f42c49c58568d9faa44139d8a21585ed95599
ricky gould <rgould2021@fau.edu>
2024-09-12 15:25:26 -0400

    app.js fix

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="app-wrapper">
       <nav className={`navbar navbar-expand-lg navbar-dark ${isScrolled ? 'scrolled' : ''}`}>
-        <a className="navbar-brand" href="#">My Portfolio</a>
+        <span className="navbar-brand">My Portfolio</span>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
